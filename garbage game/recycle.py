cabecf91f9d5f8dd02ec20a83dafcf496f6cec09
denisdutu123 <dutudenis42@gmail.com>
2025-05-10 17:47:24 +0100

    Update recycle.py

--- garbage game/recycle.py
+++ garbage game/recycle.py
@@ -8,11 +8,14 @@ HEIGHT = 800
 
 colo = (123, 67, 234)
 scor = 0
+clock = pygame.time.Clock()
+statim = time.time()
 pygame.init()
 pygame.display.set_caption("RECYCLE GAME")
 scr = pygame.display.set_mode((WIDTH, HEIGHT))
 fon = pygame.font.SysFont("Opitc", 40)
-tex = fon.render("Score:", str(scor), True, colo)
+tex = fon.render("Score:" + str(scor), True, colo)
+textim = fon.render
 
 # changing background
 def background(image):
@@ -67,34 +70,57 @@ for i in range(25):
     grou.add(imag)
     
 
-scr.blit(tex, (200, 100))
-pygame.display.update()
+
         
 
         
 # main loop function
 run = True
 while run:
+    clock.tick(30)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+    total = time.time() - statim
+    if total >= 30:
+        if scor > 6:
+            background("win.webp")
+        else:
+            background("lost.jpeg")
+        scr.blit(tex, (600, 400))
+    else:
+        
+               
+        # setting background
+        background("background.jpg")
+        timtex = fon.render("Time Left:" + str(30 - int(total)), True, "Black")
+        scr.blit(timtex, (1000, 60))
+        grou.draw(scr)
+        pygame.display.update()
+        ke = pygame.key.get_pressed()
+        if ke [K_w]:
+            bi.rect.y -=5
+        if ke [K_s]:
+            bi.rect.y +=5
+        if ke [K_a]:
+            bi.rect.x -=5
+        if ke [K_d]:
+            bi.rect.x +=5
+        #collison
+        recyite = pygame.sprite.spritecollide(bi, recycab, True)
+        nonrecyite = pygame.sprite.spritecollide(bi, nonrecycab, True)
+        
+        for i in recyite:
+            scor +=1
+            tex = fon.render("Score:" + str(scor), True, colo)
+        for n in nonrecyite:
+            scor -=2
+            tex = fon.render("Score:" + str(scor), True, colo)
             
-    # setting background
-    background("background.jpg")
-    grou.draw(scr)
+        scr.blit(tex, (130, 100))
+    
+    
     pygame.display.update()
-    ke = pygame.key.get_pressed()
-    if ke [K_w]:
-        bi.rect.y -=5
-    if ke [K_s]:
-        bi.rect.y +=5
-    if ke [K_a]:
-        bi.rect.x -=5
-    if ke [K_d]:
-        bi.rect.x +=5
-    
-    
-
 
 
 
